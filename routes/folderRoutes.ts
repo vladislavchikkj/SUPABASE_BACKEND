59316d695131425954d95fc23b232f849e806abc
Vlad <vlad.kosevichhhh@gmail.com>
2024-07-18 10:27:37 +0300

    fix versel config

--- routes/folderRoutes.ts
+++ routes/folderRoutes.ts
@@ -1,11 +1,8 @@
-import express from "express";
+import express from 'express'
+import folderController from '../controllers/folder/folderController'
 
-import folderController from "@controllers/folder/folderController";
+const router = express.Router()
+router.get('/', folderController.getRootFolder)
+router.get('/folder', folderController.getFolder)
 
-const router = express.Router();
-
-router.get('/', folderController.getRootFolder);
-router.get('/folder', folderController.getFolder);
-
-
-export default router;
+export default router
