9a345f2f6cd4c251802f70148c326a852ff2b2ce
vladislavchikkj <vlad.kosevichhhh@gmail.com>
2024-07-18 19:57:54 +0200

    fix: please help god of the universe

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import folderRoutes from './routes/folderRoutes';
 import fileRoutes from './routes/fileRoutes';
 const port = 3001;
 let corsOptions = {
@@ -15,14 +14,10 @@ const app = express();
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use('/api/folders',folderRoutes);
 app.use('/api/files',fileRoutes);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get('/api/', (req: Request, res: Response) => {
-    res.send('default root path api')
-});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- api/routes/folderRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import folderController from '../controllers/folder/folderController'
-
-const router = express.Router()
-router.get('/', folderController.getRootFolder)
-router.get('/folder', folderController.getFolder)
-
-export default router
