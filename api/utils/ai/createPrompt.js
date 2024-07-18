89b5e5c192aacbdd28e9e838c39b89578e1ae75a
vladislavchikkj <vlad.kosevichhhh@gmail.com>
2024-07-18 19:44:02 +0200

    Revert "fix: api deploy try"
    This reverts commit a8e026b95d7d3f5091f9641645d358992b99f0bb.

--- api/utils/ai/createPrompt.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPrompt = createPrompt;
-function createPrompt(prompt, text) {
-    return `${prompt}${text}`;
-}
--- api/utils/uuid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUuid = generateUuid;
-const uuid_1 = require("uuid");
-function generateUuid() {
-    return (0, uuid_1.v4)();
-}
