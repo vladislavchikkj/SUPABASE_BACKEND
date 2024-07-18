de66c24ac6af57991d400668b2cac60a980d5a15
Vlad <vlad.kosevichhhh@gmail.com>
2024-07-18 11:26:27 +0300

    fix path

--- controllers/folder/folderController.ts
+++ controllers/folder/folderController.ts
@@ -1,30 +1,33 @@
-import {Request, Response} from "express";
-import folderStorageService from "@services/folder/folderService";
-import {handleError} from "@utils/erroHandler";
+import { handleError } from '@utils/erroHandler'
+import { Request, Response } from 'express'
+import folderStorageService from '../../services/folder/folderService'
 
-const folderService = new folderStorageService();
+const folderService = new folderStorageService()
 
 class FolderController {
-    async getRootFolder(req: Request, res: Response) {
-        try {
-            const rootFolder = await folderService.listRootFoldersAndFiles();
-            res.status(200).json({success: true, data: rootFolder});
-        } catch (error) {
-            const {statusCode, message} = handleError(error);
-            res.status(statusCode).json({message});
-        }
-    }
+	async getRootFolder(req: Request, res: Response) {
+		try {
+			const rootFolder = await folderService.listRootFoldersAndFiles()
+			res.status(200).json({ success: true, data: rootFolder })
+		} catch (error) {
+			const { statusCode, message } = handleError(error)
+			res.status(statusCode).json({ message })
+		}
+	}
 
-    async getFolder(req: Request<{}, {}, {}, { folderName: string }>, res: Response) {
-        try {
-            const {folderName} = req.query;
-            const folder = await folderService.listFoldersAndFiles(folderName);
-            res.status(200).json(folder);
-        } catch (error) {
-            const {statusCode, message} = handleError(error);
-            res.status(statusCode).json({message});
-        }
-    }
+	async getFolder(
+		req: Request<{}, {}, {}, { folderName: string }>,
+		res: Response
+	) {
+		try {
+			const { folderName } = req.query
+			const folder = await folderService.listFoldersAndFiles(folderName)
+			res.status(200).json(folder)
+		} catch (error) {
+			const { statusCode, message } = handleError(error)
+			res.status(statusCode).json({ message })
+		}
+	}
 }
 
-export default new FolderController();
+export default new FolderController()
